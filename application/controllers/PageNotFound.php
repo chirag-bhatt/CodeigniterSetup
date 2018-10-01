886a4f7e06d9a2d277563fbf028f12488a2b5da7
chirag <chiragbhattsvkd@gmail.com>
2018-10-01 17:37:06 +0530

    override page not found

---
+++ application/controllers/PageNotFound.php
@@ -0,0 +1,16 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class PageNotFound extends CI_Controller {
+        
+        public function index()
+        {
+            $this->load->template('shared/pagenotfound.php');
+        }
+    
+    }
+    
+    /* End of file PageNotFound.php */
+    
+?>
---
+++ application/views/shared/pagenotfound.php
@@ -0,0 +1,3 @@
+<div class="alert alert-danger m-5" role="alert">
+    <strong>Page Not Found</strong>
+</div>
