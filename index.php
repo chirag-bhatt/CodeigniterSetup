a107c5a2da4633e77fd67801863f9ef55287882a
chirag <chiragbhattsvkd@gmail.com>
2018-10-01 15:24:23 +0530

    - Added assets folder to the root directory which contains our application assets like css/js/images. - Defined constant in index.php file so we can easily access assets folder path using RESPATH constant. ohhhhhh Thats great

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-
+	define('RESPATH',dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR);
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
